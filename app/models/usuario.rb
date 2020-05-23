4d3dbe585554e1dd835de4a9e5683e9619b86715
Jean Mendonça <pierrybm@gmail.com>
2020-05-23 00:29:17 +0000

    final project

---
+++ app/models/usuario.rb
@@ -0,0 +1,13 @@
+class Usuario < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  validates :nome, :perfil, :status, presence: true
+  enum perfil: [:Colaborador, :Gerente, :Administrador]
+  enum status: [:Ativo, :Inativo] 
+end
